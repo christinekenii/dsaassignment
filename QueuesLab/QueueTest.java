ee683dfaf5233419134df6932b12e2112447a5fc
tatiana-omolleh <102170021+tatiana-omolleh@users.noreply.github.com>
2023-07-14 14:48:43 +0300

    Add files via upload

---
+++ QueuesLab/QueueTest.java
@@ -0,0 +1,43 @@
+package QueuesLab;
+
+public class QueueTest {
+    public static void main(String[]args){
+        Queue queue1=new Queue(10); //create queue
+        queue1.insert(20);
+        queue1.insert(55);
+        queue1.insert(47);
+        queue1.insert(119);
+        queue1.insert(20);
+        queue1.insert(4);
+        queue1.insert(35);
+        queue1.insert(80);
+        queue1.insert(77);
+        queue1.insert(13);
+
+
+        //use a loop to print out the elements of the queue
+
+        for(int i=0;i<10;i++){
+            System.out.println(queue1.queArray[i]);
+        }
+        //print out values of the front and rear
+        System.out.println("This is the front: "+ queue1.peekFront());
+        System.out.println("This is the rear: "+ queue1.queArray[queue1.rear]);
+
+        queue1.insert(44);
+
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+        queue1.remove();
+
+    }
+}
--- QueuesLab/SupermarketQueue.java
+++ QueuesLab/SupermarketQueue.java
@@ -1,3 +1,4 @@
+package QueuesLab;
 
 import java.util.LinkedList;
         import java.util.Queue;
