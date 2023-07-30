e57c7a469bb0f530cc14ff54cb63e01f49dfeac8
christinekenii <christine.keni@strathmore.edu>
2023-07-30 16:25:33 +0300

    fixed question 9

---
+++ QUESTION 9/MSTApp.java
@@ -0,0 +1,18 @@
+class MSTApp {
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+        theGraph.addVertex('A'); // 0
+        theGraph.addVertex('B'); // 1
+        theGraph.addVertex('C'); // 2
+        theGraph.addVertex('D'); // 3
+        theGraph.addVertex('E'); // 4
+        theGraph.addEdge(0, 1); // AB
+        theGraph.addEdge(1, 2); // BC
+        theGraph.addEdge(0, 3); // AD
+        theGraph.addEdge(3, 4); // DE
+
+        System.out.print("Minimum Spanning Tree (MST): ");
+        theGraph.bfsMST(); // BFS to find MST
+        System.out.println();
+    } // end main()
+} // end class MSTApp
